9ce844db385353449878893a283dffa7356b86b8
Nick <nikoklis2@hotmail.gr>
2021-06-19 17:48:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -23,13 +23,6 @@ app.get("/", (req, res) => {
 });
 
 app.get("/:id", (req, res) => {
-  if (req.params.id == 5) {
-    return res.status(200).send(someData[req.params.id]);
-  }
-
-  if (req.params.id == 6) {
-    return res.status(200).send(someData[req.params.id]);
-  }
   if (someData[req.params.id]) {
     return res.status(200).send(someData[req.params.id]);
   } else {
